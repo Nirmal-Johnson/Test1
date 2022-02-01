185e0e632f23241fa5d423b41cd09644b12bb347
ADMIN <ADMIN@LAPTOP-4AUDRSQQ>
2022-02-01 12:36:52 +0530

    hi this is my first commit...

---
+++ src/SELENI/FIRSTSELENIUMPRG.java
@@ -0,0 +1,15 @@
+package SELENI;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FIRSTSELENIUMPRG
+	{
+	public static void main(String[] args) 
+	{
+	System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver.exe");
+	WebDriver d=new ChromeDriver();
+	d.get("https://mail.google.com/mail/u/0/#inbox");
+	}
+}
